497a41653538ebd48f7798906d7691f744a05041
caballeroantonio <caballeroantonio@hotmail.com>
2017-12-22 20:20:22 -0600

    ej_field_modal.xml ejemplo para referenciar objetos modales rules/date.php forma correcta que validar con J! rules ca.sql campos billetes, short_name not empty cambié history.sql, lista_campos.sql, lista_observaciones.sql, test_jt3_vs_ca.sql, view_jt3_vs_ca.sql al repositorio my_joomla ca.sql agregué objetos lsps__

--- admin/sql/history.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-t.type_title, t.content_history_options, h.version_note #, l.*
-FROM jpruebas.jos_ucm_history h
-INNER JOIN jpruebas.jos_content_types t ON t.type_id = h.ucm_type_id
-#INNER JOIN jos_jtsca_ls01s l ON l.id = h.ucm_item_id #variable dependiente de t.table
-WHERE 1
-AND t.type_id = 10100
-ORDER BY h.version_id desc;
